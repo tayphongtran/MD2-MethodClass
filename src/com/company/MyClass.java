cc1d5b9432f732975174c7b5daa1ee1736cb0363
anhtay <tayphongtran@gmail.com>
2022-03-26 23:40:31 +0700

    methodClass

---
+++ src/com/company/MyClass.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class MyClass {
+    public static void main(String[] args) {
+        Vehicel v1 = new Vehicel();
+        Vehicel v2 = new Vehicel();
+
+        v1.color = "red";
+        v2.horn();
+
+        String colorv1 = v1.color;
+    }
+}
---
+++ src/com/company/Vehicel.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Vehicel {
+    int maxxSpeed, wheels;
+    String color;
+    double feulcapacity;
+    void horn(){
+        System.out.println("beep");
+    }
+}
